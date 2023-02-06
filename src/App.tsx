f648b33e118eb11858c1f19d5a561ae754ad42e1
William <captain-donut@hotmail.com>
2023-02-06 17:12:40 +0100

    Added some styling

--- src/App.tsx
+++ src/App.tsx
@@ -23,20 +23,25 @@ const handleRemove = (index: number) => { //react hook
 
 // Return form
   return (
-    <div>
+    <div className="min-h-full flex flex-col items-baseline py-12 sm:px-6 lg:px-8 space-y-4">
 
 <h1 className="text-3xl font-bold underline">
       Todo app
     </h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={todo}
-          onChange={e => setTodo(e.target.value)} />
-        <button type="submit">Add todo entry</button>
-      </form>
-      <ul>
+    <form onSubmit={handleSubmit} className="w-full max-w-sm">
+  <div className="flex items-center border-b border-teal-500 py-2">
+    <input
+     type="text"
+     value={todo}
+     onChange={e => setTodo(e.target.value)} 
+     className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Add Todo" aria-label="Todo"/>
+    <button type="submit" className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+      Add entry
+    </button>
+  </div>
+</form>
+      <ul className ="list-disc space-y-4">
         {/* map loops over array and render each todo to <li> elements */}
         {todos.map((todo, index) => (
           <li key={index}>
