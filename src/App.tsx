f869571a76a8727720ed10266f2261b4710d5c30
William <captain-donut@hotmail.com>
2023-02-07 10:42:57 +0100

    Added done button and style

--- src/App.tsx
+++ src/App.tsx
@@ -4,53 +4,66 @@ import './App.css';
 
 const App = () => {
   const [todo, setTodo] = useState(""); //todo = value in input field. setTodo = function that updates todo value.
-  const [todos, setTodos] = useState<string[]>([]); // visible list of todos
+  const [todos, setTodos] = useState<{ text: string, done: boolean }[]>([]); // visible list of todos. Hold both text and done states to keep track.
 
   // -- Submit --
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { //react hook
     e.preventDefault(); //don't refresh page
-    setTodos([todo, ...todos]); // latest added appear first
+    setTodos([{ text: todo, done: false }, ...todos]) // latest added appear first
     setTodo(""); //reset after each submit
   };
 
-// -- Remove --
-const handleRemove = (index: number) => { //react hook
-  let newTodos: string[] = [...todos]; //get the todos
-  newTodos.splice(index, 1); //remove 
-  setTodos(newTodos); //update
-};
+  // -- Remove --
+  const handleRemove = (index: number) => { //react hook, list index
+    let currentTodos: { text: string, done: boolean }[] = [...todos]; //get the todos
+    currentTodos.splice(index, 1); //remove 
+    setTodos(currentTodos); //update state
+  };
+
+  // -- Done --
+  const handleDone = (index: number) => {
+    let currentTodos: { text: string, done: boolean }[] = [...todos]; //get the todos
+    currentTodos[index].done = !currentTodos[index].done;
+    setTodos(currentTodos);
+  };
 
 
-// Return form
+  // Return the site:
   return (
     <div className="min-h-full flex flex-col items-baseline py-12 sm:px-6 lg:px-8 space-y-4">
 
-<h1 className="text-3xl font-bold underline">
-      Todo app
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Todo app
+      </h1>
 
-    <form onSubmit={handleSubmit} className="w-full max-w-sm">
-  <div className="flex items-center border-b border-teal-500 py-2">
-    <input
-     type="text"
-     value={todo}
-     onChange={e => setTodo(e.target.value)} 
-     className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Add Todo" aria-label="Todo"/>
-    <button type="submit" className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
-      Add entry
-    </button>
-  </div>
-</form>
-      <ul className ="list-disc space-y-4">
-        {/* map loops over array and render each todo to <li> elements */}
+      {/* form */}
+      <form onSubmit={handleSubmit} className="w-full max-w-sm">
+        <div className="flex items-center border-b border-teal-500 py-2">
+          <input
+            type="text"
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+            className="input-css" placeholder="Add Todo" aria-label="Todo" />
+          <button type="submit" className="add-button">
+            Add entry
+          </button>
+        </div>
+      </form>
+
+      {/* list */}
+      <ul className="list-disc space-y-4">
+        {/* map loops over array and render each todo to <li> elements. line through if marked as complete */}
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" 
+          <li key={index} style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+            {todo.text}
+            <button type="button" className="done-button"
+              onClick={() => handleDone(index)}> Mark as done </button>
+            <button type="button" className="delete-button"
               onClick={() => handleRemove(index)}> Delete </button>
           </li>
         ))}
       </ul>
+
     </div>
   );
 };
